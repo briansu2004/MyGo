9509ae6e8934ed64958457b7ecc06c613bc4efc4
Brian Su <briansu2004@hotmail.com>
2023-09-03 15:57:39 -0400

    $

---
+++ IfElseHelloWorld/go.mod
@@ -0,0 +1,3 @@
+module github.com/briansu2004/ifelsehelloworld
+
+go 1.20
---
+++ IfElseHelloWorld/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	if fmt.Print("hello "); true {
+		fmt.Print("world")
+	}
+}
